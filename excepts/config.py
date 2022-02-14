07a9469a7bd63a41f9d39ed0c08e926e53dcf91f
Maxinoxx <o.tru08@gmail.com>
2022-02-14 15:15:38 -0600

    Fin de las katas

---
+++ excepts/config.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        configuration = open('config.py')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+
